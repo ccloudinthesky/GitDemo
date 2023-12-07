4025de9620d1663fc47cc32d3bdb11f5c690a574
ccloudinthesky <150308333+ccloudinthesky@users.noreply.github.com>
2023-12-07 20:45:34 +0800

    Update helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // Todo: 寫一個印出"Hello World"的程式
 int main() {
   cout << "Hello World" << endl;
-  for(int i = 0; i <3; i++)
+  for(int i = 0; i < 2; i++)
     cout << "Hello World" << endl;
   return 0;
 }
